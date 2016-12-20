83ad2ff2702c14a2817f546b69e794f14d52b83b
SensehacK <kautilyasave@gmail.com>
2016-12-20 19:14:19 +0530

    Updated OpenWeather Constants

--- Tourist Helper/OpenWeatherConstants.swift
+++ Tourist Helper/OpenWeatherConstants.swift
@@ -8,3 +8,26 @@
 
 import Foundation
 
+struct OpenWeatherConstants {
+    
+    // Sample URL
+    
+    // URL:http://api.openweathermap.org/data/2.5/weather?q=vasai&APPID=b25ecd39a0f08f16aed86d6ffaafc964
+    
+    
+    
+   // openweathermap.org/data/2.5/weather?q=vasai&APPID=b25ecd39a0f08f16aed86d6ffaafc964
+    
+    static let APIScheme = "http"
+    static let APIHost = "api.openweathermap.org"
+    static let APIPath = "/data/2.5/weather?q="
+    
+    
+    // Api Request URL
+    static let AppID = "b25ecd39a0f08f16aed86d6ffaafc964"
+    static let City = (-180.0, 180.0)
+    static let Latitude = 1.0
+    static let Longitude = 2.0
+   
+
+}
